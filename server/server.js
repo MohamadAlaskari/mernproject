4452a841a133e0f3de427f834685064d0968996a
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-14 22:51:48 +0100

    mernproject

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app= express()
+
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://Mohamad:96MoAl96@cluster0.wujtyez.mongodb.net/mernproject?retryWrites=true&w=majority")
+
+
+app.get("/",(req, res)=>{
+    res.send("test")
+}) 
+
+app.listen("3001",()=>{
+    console.log("Server Work good")
+})
