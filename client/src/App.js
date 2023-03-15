54c41f5a3e36679108881c62d0ff7116c58fce14
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-15 13:17:08 +0100

    mern

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
 import { useState, useEffect } from "react"
 import Axios from "axios"
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Form, Button } from 'react-bootstrap';
+
+
 export default function App() {
 
+  const api = "http://localhost:3001"
   const [users, setUsers] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [email, setEmail] = useState("")
 
-  useEffect(()=>{
-    Axios.get("http://localhost:3001/users")
-    .then(res => {
-      setUsers(res.data)
-    })
-    .catch(error => {
-      // handle error
-    });
-  },[])
 
+  useEffect(() => {
+    Axios.get(`${api}/users`)
+      .then(res => { setUsers(res.data) })
+      .catch(error => {
+        // handle error
+      });
+  }, [users])
+
+  const createUser = () => {
+    if (name && age && email) {
+      Axios.post(`${api}/createUser`, {
+        name: name,
+        age: age,
+        email: email
+      })
+        .then(res => { console.log(res.data) })
+        .catch(error => {
+          // handle error
+        });
+    }
+
+  }
 
   /*
     const users = [
@@ -25,10 +48,12 @@ export default function App() {
 
 
   return (
-    <>
+    <Container>
+
+
       {users.map(user => {
         return (
-          <div className="card">
+          <div className="card" key={user._id} >
             <ul>
               <li>Name: {user.name}</li>
               <li>Age: {user.age}</li>
@@ -38,7 +63,33 @@ export default function App() {
           </div>
         )
       })}
-    </>
+      <div>
+        <input type="text" placeholder="Name" onChange={e => setName(e.target.value)} />
+        <input type="" number placeholder="Age" onChange={e => setAge(e.target.value)} />
+        <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+        <button onClick={createUser}>Creat User</button>
+      </div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail" >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,28 +1,43 @@
 // creat server
 const express = require("express")
-
 const app = express()
-
 // Allow cross-origin requests
 const cors = require("cors")
 app.use(cors());
 
+//wir keine leere data in consol.log() bei req.data in app.js  
+app.use(express.json())
+
+
 
 // connect to DB
+const username = process.env.USERNAME,
+    password = process.env.PASSWORD,
+    database = process.env.DATABASE;
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://Mohamad:96MoAl96@cluster0.wujtyez.mongodb.net/mernproject?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.wujtyez.mongodb.net/${database}?retryWrites=true&w=majority`)
 
-
+/*Models*/
 // import UserModel
 const UserModel = require('./models/Users')
 
+// get request
 app.get("/users", async (req, res) => {
     const users = await UserModel.find();
     res.json(users)
 })
 
+// post request create user
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.json(req.body)
+})
+
 
 // creat port
-app.listen("3001", () => {
+const _port = "3001"
+app.listen(_port, () => {
     console.log("Server Work")
 })
